9eabf6c60c327f04c018dbe22b6bb23c54ad1f32
Amit <amit2.kumar@paytm.com>
2015-03-26 17:23:44 +0530

    Moving updateValue outside the attach event.

--- 2WayDataBinding.js
+++ 2WayDataBinding.js
@@ -8,47 +8,46 @@
  * */
 var $scope = {};
 (function () {
-    var bindClasses = ["name", "age"];
-    var attachEvent = function (classNames) {
-        classNames.forEach(function (className) {
-            var elements = document.getElementsByClassName(className);
-            var value;
-            for (var index in elements) {
-                if(elements.hasOwnProperty(index)) {
-                    if(elements[index].tagName == "INPUT") {
-                        elements[index].onkeyup = function () {
-                            for (var index in elements) {
-                                if(elements.hasOwnProperty(index)) {
-                                    switch (elements[index].tagName) {
-                                        case "INPUT":
-                                            elements[index].value = this.value;
-                                            break;
-                                        default :
-                                            elements[index].innerHTML = this.value;
-                                            break;
-                                    }
-                              }
-                          }
-                          value = this.value;
-                        }
-                    }
-                }
+  var bindClasses = ["name", "age"];
+  var updateValue = function(elements, value) {
+    for (var index in elements) {
+      if (elements.hasOwnProperty(index)) {
+        switch (elements[index].tagName) {
+          case "INPUT":
+            elements[index].value = value;
+            break;
+          default :
+            elements[index].innerHTML = value;
+            break;
+        }
+      }
+    }
+  };
+  var attachEvent = function (classNames) {
+    classNames.forEach(function (className) {
+      var elements = document.getElementsByClassName(className);
+      var value;
+      for (var index in elements) {
+        if (elements.hasOwnProperty(index)) {
+          if (elements[index].tagName == "INPUT") {
+            elements[index].onkeyup = function () {
+              updateValue(elements, this.value);
+              value = this.value;
             }
-            Object.defineProperty($scope, className, {
-                set: function (newValue) {
-                    for (var index in elements) {
-                        if(elements.hasOwnProperty(index)) {
-                            elements[index].value = newValue;
-                        }
-                    }
-                    value = newValue;
-                },
-                get: function() {
-                    return value;
-                }
-            });
+          }
+        }
+      }
+      Object.defineProperty($scope, className, {
+        set: function (newValue) {
+          updateValue(elements, newValue);
+          value = newValue;
+        },
+        get: function () {
+          return value;
+        }
+      });
 
-        });
-    };
-    attachEvent(bindClasses);
+    });
+  };
+  attachEvent(bindClasses);
 })();
