e4e996642f4c19717ea56e4c6e7c6c7f0e70e4a0
Namita Malik <namita.malik@paytm.com>
2015-03-26 14:43:02 +0530

    Introducing $scope functionality with vanilla JavaScript.

--- 2WayDataBinding.js
+++ 2WayDataBinding.js
@@ -1,16 +1,33 @@
 /**
  * Created by Namita Malik on 26/3/15.
  */
-var attachEvent = function (classNames) {
-    classNames.forEach(function (className) {
-        var elements = document.getElementsByClassName(className);
-        for (var index in elements) {
-            elements[index].onkeyup = function () {
-                for (var index in elements) {
-                    elements[index].value = this.value;
+
+/*
+ * I am here polluting the global object by making $scope as global scope variable, but this for the testing purpose
+ * and sake of simplicity so that I can test from terminal
+ * */
+var $scope = {};
+(function () {
+    var bindClasses = ["name", "age"];
+    var attachEvent = function (classNames) {
+        classNames.forEach(function (className) {
+            var elements = document.getElementsByClassName(className);
+            for (var index in elements) {
+                elements[index].onkeyup = function () {
+                    for (var index in elements) {
+                        elements[index].value = this.value;
+                    }
                 }
             }
-        }
-    });
-};
-attachEvent(["name", "age"]);
+            Object.defineProperty($scope, className, {
+                set: function (newValue) {
+                    for (var index in elements) {
+                        elements[index].value = newValue;
+                    }
+                }
+            });
+
+        });
+    };
+    attachEvent(bindClasses);
+})();
